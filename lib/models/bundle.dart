65b562a40af6b992cc355ee16b8164517c7c8d43
Muhammad Adeel Abid <cmkali2126@gmail.com>
2026-02-24 13:08:30 +0500

    feat: create Bundle and Plan models

---
+++ lib/models/bundle.dart
@@ -0,0 +1,15 @@
+class Bundle {
+  final String name;
+  final String data;
+  final String validity;
+  final double price;
+  final String type;
+
+  Bundle({
+    required this.name,
+    required this.data,
+    required this.validity,
+    required this.price,
+    required this.type,
+  });
+}
---
+++ lib/models/plan.dart
@@ -0,0 +1,13 @@
+class Plan {
+  final String name;
+  final String data;
+  final String validity;
+  final double price;
+
+  Plan({
+    required this.name,
+    required this.data,
+    required this.validity,
+    required this.price,
+  });
+}
